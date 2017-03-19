d7f16c4cbaca8c638aa5bb7111d9f1f17e46a31d
BuildTools <unconfigured@null.spigotmc.org>
2017-03-19 11:27:53 +0100

    Importado bootstrap, hechas index.html y faq.html

--- src/main/java/aiss/HelloWorldServlet.java
+++ src/main/java/aiss/HelloWorldServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
 	private static final Logger log = Logger.getLogger(HelloWorldServlet.class.getName());
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
